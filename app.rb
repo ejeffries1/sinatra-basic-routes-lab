9aa4cbf36f619d9f19a22c0734d40962af0bd628
evan <ejeffries14@gmail.com>
2021-10-09 02:30:56 -0400

    commit tree 66d1e353fca21dddb41eff7009073765a856756b

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    resp = Rack::Response.new
+
+    get '/name' do
+        "My name is Evan"
+    end
+    resp.status = 200
+
+    get '/hometown' do
+        "My hometown is Jackson"
+    end
+    resp.status = 200
+
+    get '/favorite-song' do
+        "My favorite song is __"
+    end
+    resp.status = 200
 end
